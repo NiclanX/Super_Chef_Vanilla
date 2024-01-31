834aaf87a90d409662f15378af48aeeec3d66a09
NiclanX <nicardolannaman@gmail.com>
2024-01-30 22:39:15 -0500

    1/30/2024 last

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
+fetch('data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Map over the recipes array and create HTML representation for each recipe
+        const recipeHTML = data.recipes.map(recipe => {
+            return `
+                <a href="${recipe.page_url}">
+                    <div id="heroCard">
+                        <img src="${recipe.image_url}" alt="" loading="lazy">
+                        <h4>${recipe.title}</h4>
+                        <p>Preptime / Serving: 30mins</p>
+                    </div>      
+                </a>
+                
+            `;
+        }).join(''); // Convert array of HTML strings to a single string
 
-       let data1 = 'data.json'
-       
-       // Fetch JSON data
-        fetch(data1) // Assuming data.json is in the same directory as this HTML file
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('JSON data:', data);
-                // Process JSON data here
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            });
+        // Assuming there's a container with id="recipe-container" where you want to display the recipes
+        document.getElementById('sorter').innerHTML = recipeHTML;
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
